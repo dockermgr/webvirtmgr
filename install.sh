1cfa3439488d97ed8c4688aa8add1635e124ad6b
casjay <git-admin@casjaysdev.com>
2023-08-10 20:18:15 -0400

    🗃️ Updated domain names 🗃️
    install.sh
    install.sh.bak
    LICENSE.md

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ##@Version           :  202305031641-git
 # @@Author           :  Jason Hempstead
-# @@Contact          :  jason@casjaysdev.com
+# @@Contact          :  jason@casjaysdev.pro
 # @@License          :  WTFPL
 # @@ReadME           :  install.sh --help
 # @@Copyright        :  Copyright: (c) 2023 Jason Hempstead, Casjays Developments
